8e3bf2d7bdfb1679a39e224409503c7185b68b0e
Sarthak <sarthakbhatt@live.com>
2023-10-18 14:04:20 +0530

    Styled active links in navbar

---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <title>Issues</title>
+      <div>Issues page</div>;
+    </>
+  );
+};
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <title>Dashboard</title>
+      <div>Hello world</div>;
+    </>
+  );
 }
